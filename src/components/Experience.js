9081a7675791dd76d93aefe7a18f680a8703f0cf
Gary Gary <jordankelwick@gmail.com>
2019-03-08 18:20:35 +0000

    speeling mistakes

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -7,7 +7,7 @@ const subtext = "I've done stuff?";
 const content = (<div>
     <body>I have worked...</body>
     <ul class="wrap">
-        <li>At {GilmondLink} as a QA Test Professional <em>(10/2017 - 03/2018)</em></li>
+        <li>At {GilmondLink} as a QA Test Professional <em>(10/2017 - 03/2019)</em></li>
         {/* <li>At {GilmondLink} (again!) as a Software Developer to present day <em>(03/2018 - now!)</em></li> */}
     </ul>
 </div>);
