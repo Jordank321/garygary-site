1272d1a9bb2fdde35b7ab2d4199347a22cb012ed
Jordank321 <jordankelwick@gmail.com>
2019-04-02 20:31:02 +0100

    Ooops

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -8,7 +8,7 @@ const content = (<div>
     <body>I have worked...</body>
     <ul class="wrap">
         <li>At {GilmondLink} as a QA Test Professional <em>(10/2017 - 03/2019)</em></li>
-        <li>At {GilmondLink} (again!) as a Software Developer to present day <em>(03/2018 - now!)</em></li>
+        <li>At {GilmondLink} (again!) as a Software Developer to present day <em>(04/2018 - now!)</em></li>
     </ul>
 </div>);
 
