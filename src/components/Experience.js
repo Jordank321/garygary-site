26c6bfdf330a21aa5efa6dc4a8f39faf1fc9eab4
Jordank321 <jordankelwick@gmail.com>
2019-04-29 21:48:52 +0100

    Uncomment dev role at gilmond

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,14 +2,21 @@ import { h } from 'hyperapp';
 import Section from './Section';
 import { GilmondLink } from './Links';
 
-const heading = "My Experience";
+const heading = 'My Experience';
 const subtext = "I've done stuff?";
-const content = (<div>
+const content = (
+  <div>
     <body>I have worked...</body>
     <ul class="wrap">
-        <li>At {GilmondLink} as a QA Test Professional <em>(10/2017 - 03/2019)</em></li>
-        {/* <li>At {GilmondLink} (again!) as a Software Developer to present day <em>(04/2018 - now!)</em></li> */}
+      <li>
+        At {GilmondLink} as a QA Test Professional <em>(10/2017 - 03/2019)</em>
+      </li>
+      <li>
+        At {GilmondLink} (again!) as a Software Engineer to present day{' '}
+        <em>(04/2018 - now!)</em>
+      </li>
     </ul>
-</div>);
+  </div>
+);
 
-export default () => Section(heading, content, subtext)
+export default () => Section(heading, content, subtext);
